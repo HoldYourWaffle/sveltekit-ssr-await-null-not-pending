88f4bc5bfdc4fec677cbd4baa69517e953ed4c50
HoldYourWaffle <ravivanrooijen@live.nl>
2023-10-17 18:14:42 +0200

    Less naive stored async component (SSR renders fullfiled branch)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,27 @@
 <script lang="ts">
-    let ClientOnlyComponent = import("$lib/ClientOnlyComponent.svelte")
+    import type { ComponentType } from "svelte"
+    import { browser } from "$app/environment"
+    import type ClientOnlyComponent from "$lib/ClientOnlyComponent.svelte"
+
+    let ClientOnlyComponent: Promise<ComponentType<ClientOnlyComponent>>
+    if (browser) {
+        ClientOnlyComponent = import("$lib/ClientOnlyComponent.svelte").then((module) => module.default)
+    }
 </script>
 
 <svelte:head>
     <title>Hello World!</title>
 </svelte:head>
 
+<!--
+    FIXME during SSR ClientOnlyComponent will remain undefined
+    This renders the fullfiled branch of the await block,
+    even though the documentation explicitly states that only the pending state will be rendered on the server.
+    See: https://svelte.dev/docs/logic-blocks#await
+-->
 {#await ClientOnlyComponent}
     <p>Loading...</p>
 {:then ClientOnlyComponent}
     <p>Loaded!</p>
-    <svelte:component this={ClientOnlyComponent.default} />
+    <svelte:component this={ClientOnlyComponent} />
 {/await}
